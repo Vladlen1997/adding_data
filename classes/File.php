7fd5034c3694499f96faee3e1f890c95fc535e54
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-14 11:33:57 +0300

    i assign the $file variable to the method parameter and check for access using the is_writable() method.

--- classes/File.php
+++ classes/File.php
@@ -5,11 +5,18 @@ class File
     public $fp;  //opened file
     public $file; //the way of file
 
-    public function __construct($file) {
+    public function __construct($file)
+    {
+        $this->file = $file;
+        if (!is_writable($this->file)) {
+            echo "Файл  {$this->file} недоступен для записи";
+            exit;
+        }
 
     }
 
-    public function __destruct() {
+    public function __destruct()
+    {
 
     }
 
