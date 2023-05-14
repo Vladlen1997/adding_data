ce3846fbf18fd4903728ef3120ae5e99a52e0655
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-14 11:56:55 +0300

    added more text to $file

--- index.php
+++ index.php
@@ -5,3 +5,7 @@ require_once 'classes/File.php';
 $file = new File(__DIR__ . '/file.txt');
 
 $file->write('Привет всем!');
+$file->write('Привет всем!');
+$file->write('Привет всем!');
+$file->write('Привет всем!');
+$file->write('Привет всем!');
